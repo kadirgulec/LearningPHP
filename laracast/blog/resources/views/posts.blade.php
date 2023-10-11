86ac9480eacf6a3e033f0236cbd802cccbd8dc95
Praktikant Entwicklung <praktikant.ent@AKS-SERVICE.local>
2023-10-11 11:54:59 +0200

    episode 32

--- laracast/blog/resources/views/posts.blade.php
+++ laracast/blog/resources/views/posts.blade.php
@@ -1,6 +1,34 @@
-@extends('/components/layout')
+<x-layout>
 
-@section('content')
+    @include ('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+
+@if ($posts->count())
+ <x-post-featured-card :post='$posts[0]' />
+
+        <div class="lg:grid lg:grid-cols-2">
+            @foreach ($posts->skip(1) as $post)
+                <x-post-card :post="$post" />
+            @endforeach
+
+
+@else
+   <p class="text-center">No Posts.</p>
+@endif
+
+
+        </div>
+
+        {{-- <div class="lg:grid lg:grid-cols-3">
+
+            <x-post-card></x-post-card>
+            <x-post-card></x-post-card>
+            <x-post-card></x-post-card>
+        </div> --}}
+    </main>
+
+    {{-- @section('content')
 
     @foreach ($posts as $post)
         <article>
@@ -21,4 +49,5 @@
         </article>
     @endforeach
 
-@endsection
+@endsection --}}
+</x-layout>
--- laracast/blog/routes/web.php
+++ laracast/blog/routes/web.php
@@ -24,7 +24,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest()
+        'posts' => Post::latest()->get()
     ]);
 
 });
