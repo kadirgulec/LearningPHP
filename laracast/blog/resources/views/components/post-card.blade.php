2937696017621a309bd80a1037d893fda22dbed2
Praktikant Entwicklung <praktikant.ent@AKS-SERVICE.local>
2023-10-11 16:07:12 +0200

    Episode 34 finished

--- laracast/blog/resources/views/components/post-card.blade.php
+++ laracast/blog/resources/views/components/post-card.blade.php
@@ -1,20 +1,18 @@
 @props(['post'])
+
+
 <article
-    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    {{$attributes->merge(['class'=>"transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl"])}}>
     <div class="py-6 px-5">
         <div>
+            {{--TODO--}}
             <img src="/images/illustration-3.png" alt="Blog Post illustration" class="rounded-xl">
         </div>
 
         <div class="mt-8 flex flex-col justify-between">
             <header>
                 <div class="space-x-2">
-                    <a href="/categories/{{$post->category->slug}}"
-                        class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">{{$post->category->name}}</a>
-                    {{-- <a href="#"
-                        class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">Updates</a> --}}
+                    <x-category-button :$post/>
                 </div>
 
                 <div class="mt-4">
@@ -43,12 +41,12 @@
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">{{$post->author->name}}</h5>
-                        <h6>Mascot at Laracasts</h6>
+                        {{-- <h6>Mascot at Laracasts</h6> --}}
                     </div>
                 </div>
 
                 <div>
-                    <a href="#"
+                    <a href="/posts/{{$post->slug}}"
                         class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read
                         More</a>
                 </div>
--- laracast/blog/resources/views/posts.blade.php
+++ laracast/blog/resources/views/posts.blade.php
@@ -4,50 +4,13 @@
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-@if ($posts->count())
- <x-post-featured-card :post='$posts[0]' />
+        @if ($posts->count())
+            <x-post-featured-card :post='$posts[0]' />
 
-        <div class="lg:grid lg:grid-cols-2">
-            @foreach ($posts->skip(1) as $post)
-                <x-post-card :post="$post" />
-            @endforeach
+            <x-posts-grid :$posts/> {{--shorthand of :posts = "$posts"--}}
+        @else
+            <p class="text-center">No Posts.</p>
+        @endif
 
-
-@else
-   <p class="text-center">No Posts.</p>
-@endif
-
-
-        </div>
-
-        {{-- <div class="lg:grid lg:grid-cols-3">
-
-            <x-post-card></x-post-card>
-            <x-post-card></x-post-card>
-            <x-post-card></x-post-card>
-        </div> --}}
     </main>
-
-    {{-- @section('content')
-
-    @foreach ($posts as $post)
-        <article>
-
-            <h1>
-                <a href="/posts/{{$post->slug}}">
-                    {{$post->title}}
-                </a>
-            </h1>
-            <p>
-               By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-            </p>
-
-            <div>
-                {{$post->excerpt}}
-            </div>
-
-        </article>
-    @endforeach
-
-@endsection --}}
 </x-layout>
