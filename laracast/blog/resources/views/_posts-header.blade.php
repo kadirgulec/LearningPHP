6590e495f2ca5672af0a04eda6d4bf98b43fff30
Praktikant Entwicklung <praktikant.ent@AKS-SERVICE.local>
2023-10-11 16:16:00 +0200

    trying pull

--- laracast/blog/resources/views/_posts-header.blade.php
+++ laracast/blog/resources/views/_posts-header.blade.php
@@ -53,13 +53,7 @@
 
                 </div>
             </div>
-            {{-- <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                <option value="category" disabled selected>Category
-                </option>
-                @foreach ($categories as $category)
-                    <option value="{{ $category->slug }}">{{ $category->name }}</option>
-                @endforeach
-            </select> --}}
+
 
 
         </div>
