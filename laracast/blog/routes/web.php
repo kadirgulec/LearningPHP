78ec85381d60d7e85f6f9a34f6a927c3e34abed4
Praktikant Entwicklung <praktikant.ent@AKS-SERVICE.local>
2023-10-13 16:29:01 +0200

    Newsletters are going to be added

--- laracast/blog/routes/web.php
+++ laracast/blog/routes/web.php
@@ -25,17 +25,41 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
-Route::post('posts/{post:slug}/comments',[PostCommentsController::class, 'store']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
-Route::get('register',[RegisterController::class , 'create'])->name('create')->middleware('guest');
-Route::post('register',[RegisterController::class , 'store'])->middleware('guest');
+Route::get('register', [RegisterController::class, 'create'])->name('create')->middleware('guest');
+Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
 Route::get('/login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('/login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('/logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
+Route::post('/newsletter', function () {
+
+    request()->validate(['email'=>'required|email']);
+    // require_once('/path/to/MailchimpMarketing/vendor/autoload.php');
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us11'
+    ]);
+
+    try
+    {
+        $response = $mailchimp->lists->addListMember('b69e8dd745', [
+            "email_address" => request('email'),
+            "status" => "subscribed",]);
+    }catch(\Exception $e){
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added to our newsletter list',
+        ]);
+    }
+
+
+    return redirect('/')->with('success', 'You are now subscribed for our newsletter!');
+});
 
 Route::get('/test', [HomeController::class, 'test']);
-
-
